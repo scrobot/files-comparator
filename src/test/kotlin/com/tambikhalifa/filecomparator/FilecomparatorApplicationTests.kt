715caada61e977c003bfa0b7f29960d39ccc8229
Alex Scrobot <scrobot91@gmail.com>
2019-10-14 16:45:31 +0700

    init

---
+++ src/test/kotlin/com/tambikhalifa/filecomparator/FilecomparatorApplicationTests.kt
@@ -0,0 +1,16 @@
+package com.tambikhalifa.filecomparator
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class FilecomparatorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
